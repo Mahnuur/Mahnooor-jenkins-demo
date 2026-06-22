29348ec287cbf9e73012a24c2afc885d401534c6
Mahnuur <mahnur735@gmail.com>
2026-06-22 15:21:44 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const http = require('http');
+
 http.createServer((req, res) => {
-res.writeHead(200);
-res.send("CI/CD Pipeline Working ✔ Version 2 Triggered by GitHub Webhook");
-}).listen(3000, () => console.log('Running on port 3000'));
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("CI/CD Pipeline Working ✔ Version 2 Triggered by GitHub Webhook");
+}).listen(3000, () => {
+    console.log('Running on port 3000');
+});
