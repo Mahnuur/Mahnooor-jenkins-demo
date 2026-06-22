576e53bb6fda613830195e1418b85db6e599afa6
Mahnuur <mahnur735@gmail.com>
2026-06-22 15:56:57 +0500

    webhook test v3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("CI/CD Pipeline Working ✔ Version 2 Triggered by GitHub Webhook");
+    res.end("🚀 Jenkins CI/CD Pipeline LIVE - Webhook Trigger Verified");
 }).listen(3000, () => {
     console.log('Running on port 3000');
 });
